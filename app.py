f4b6c5987e78119c359f9707bd0ecf5fd0f36b49
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2021-06-11 11:57:55 -1000

    added template folder and changed route

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+# Route for the index page 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, world!</p>"
+def index(name=None):
+    return render_template('index.html', name=name)
