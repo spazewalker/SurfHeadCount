d74bece0fa9868526be2e43a9269078c266906f3
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2021-06-11 13:09:55 -1000

    added handler for POST requests

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 # Route for the index page 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index(name=None):
-    return render_template('index.html', name=name)
+    if request.method == 'GET':
+        return render_template('index.html', name=name)
+    else:
+        # This is where we can handle the POST request to pytorch
+        print("Request made: " + request.method)
+        print("Not implemented yet")
+        return render_template('index.html', name=name)
